4048e3e8277513feee7bae6969ec15f55eb7760c
zhushuai0616 <zhushuai0616@126.com>
2020-06-19 22:07:32 +0800

    发布项目JavaProject

---
+++ JavaProject/src/zhus/com/test/JavaTestGit.java
@@ -0,0 +1,20 @@
+package zhus.com.test;
+
+public class JavaTestGit {
+
+	public static void main(String[] args) {
+
+		String a = null;
+		System.out.println(null == a);
+		System.out.println(a == null);
+		System.out.println(a == "");
+		System.out.println("".equals(a));//
+		System.out.println(a.equals(""));
+		
+		
+		
+		
+		
+	}
+
+}
